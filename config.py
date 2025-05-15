59ec678b24346d17a53f733fcd2c07242720fd96
Ic0n <gelesom30@gmail.com>
2025-05-15 22:48:57 +0300

    -

--- config.py
+++ config.py
@@ -1,4 +1,17 @@
 """Модуль конфигураций."""
+import pygame as pg
 
 # Игровые размеры
 SIZE_WINDOW = (1100, 700)
+
+# Шрифты
+pg.font.init()
+FONT_BUTTON = pg.font.Font(None, 50)
+FONT_QUESTIONBOX = pg.font.Font(None, 70)
+
+# Цвета
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
